91546315dbfc255be2b28b43d1072289503c06d9
chuxuezhe11 <hanjixiao@huawei.com>
2021-06-03 19:00:17 +0800

    delete the content in run-buildtools-binary.sh

--- trace_analyzer/tools/run-buildtools-binary.sh
+++ trace_analyzer/tools/run-buildtools-binary.sh
@@ -11,29 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-set -e
-
-PROJECT_ROOT="$(cd -P ${BASH_SOURCE[0]%/*}/..; pwd)"
-
-if [[ -n "$BASH_VERSION" && "${BASH_SOURCE:-$0}" == "$0" ]]; then
-  echo "ERROR: ${BASH_SOURCE:-$0} must be sourced"
-  exit 1
-fi
-
-case "$(uname -s | tr '[A-Z]' '[a-z]')" in
-  linux)
-    OS="linux64";;
-  *)
-    echo "Unsupported platform"
-    exit 1
-    ;;
-esac
-
-BIN_PATH="$PROJECT_ROOT/buildtools/$OS/$CMD"
-if [ ! -x "$BIN_PATH" ]; then
-  echo "Cannot find $BIN_PATH. Run tools/install-build-deps first."
-  exit 1
-fi
-
-exec "$BIN_PATH" "$@"
