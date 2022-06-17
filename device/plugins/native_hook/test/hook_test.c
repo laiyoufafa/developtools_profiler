928c4fe23abf454dbe0a4c8ed3a7653017518c77
shuxinyia <shuxinyi4@h-partners.com>
2022-06-17 14:27:38 +0800

    修改相关的告警问题      Signed-off-by: shuxinyiA <zhangcui11@huawei.com>
    Signed-off-by: shuxinyia <shuxinyi4@h-partners.com>

--- device/plugins/native_hook/test/hook_test.c
+++ device/plugins/native_hook/test/hook_test.c
@@ -184,7 +184,7 @@ void ApplyForRealloc(int mallocSize)
     g_freeDuration += (double)(timerStop - timerStart) / CLOCKS_PER_SEC;
     printf("free success, free time is %f\n", (double)(timerStop - timerStart) / CLOCKS_PER_SEC);
     printf("realloc apply success, total time is %f\n", duration);
-    if (p) {
+    if (p != NULL) {
         free(p);
         p = NULL;
     }
