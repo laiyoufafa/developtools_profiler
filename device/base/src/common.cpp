dffbefe0a8066805be36bdb2b8f056dc1b913bb1
zhangcui <zhangcui11@huawei.com>
2022-10-18 08:26:25 +0000

    update device/base/src/common.cpp.
    Signed-off-by: zhangcui <zhangcui11@huawei.com>

--- device/base/src/common.cpp
+++ device/base/src/common.cpp
@@ -24,11 +24,12 @@
 
 namespace COMMON {
 constexpr int EXECVP_ERRNO = 2;
+const int SHELL_UID = 2000;
 const std::string DEFAULT_PATH = "/data/local/tmp/";
 
 bool IsProcessRunning()
 {
-    setgid(2000);
+    setgid(SHELL_UID);
     char buffer[PATH_MAX + 1] = {0};
     readlink("/proc/self/exe", buffer, PATH_MAX);
     std::string processName = buffer;
