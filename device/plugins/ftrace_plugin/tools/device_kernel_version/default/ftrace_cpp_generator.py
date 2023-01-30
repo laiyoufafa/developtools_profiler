cad412377ebeabc80b800b490865f3d812471dba
zhangcui <zhangcui11@huawei.com>
2023-01-30 11:51:17 +0000

    update
    Signed-off-by: zhangcui <zhangcui11@huawei.com>

--- device/plugins/ftrace_plugin/tools/device_kernel_version/default/ftrace_cpp_generator.py
+++ device/plugins/ftrace_plugin/tools/device_kernel_version/default/ftrace_cpp_generator.py
@@ -393,7 +393,7 @@ class EventFormatterCodeGenerator(FtraceEventCodeGenerator):
             elif (event.name == "ext4_find_delalloc_range") :
                 event.print_fmt = str.replace(event.print_fmt, "REC->found_blk", "msg.found_blk()")
             elif (event.name == "mm_filemap_add_to_page_cache") | (event.name == "mm_filemap_delete_from_page_cache"):
-                event.print_fmt = "\"" + event.name + ": dev %\" PRIu64 \":%\" PRIu64 \" ino %p page=%s pfn=%\" PRIu64 \" ofs=%\" PRIu64 \"\", (((msg.s_dev()) >> 20)), (((msg.s_dev()) & ((1U << 20) - 1))), msg.i_ino(), \"0000000000000000\", msg.pfn(), msg.index() << 12"
+                event.print_fmt = "\"{}: dev %\" PRIu64 \":%\" PRIu64 \" ino %p page=%s pfn=%\" PRIu64 \" ofs=%\" PRIu64 \"\", (((msg.s_dev()) >> 20)), (((msg.s_dev()) & ((1U << 20) - 1))), msg.i_ino(), \"0000000000000000\", msg.pfn(), msg.index() << 12".format(event.name)
             elif (event.name == "ipi_raise") :
                 event.print_fmt = str.replace(event.print_fmt, "target_mask=%s", "target_mask=%\" PRIu64 \"")
                 event.print_fmt = str.replace(event.print_fmt, "__get_bitmask(target_cpus)", "msg.target_cpus()")
