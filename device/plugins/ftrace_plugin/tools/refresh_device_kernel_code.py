d8a31a1fde16f901a2cfcc9330d8e810acb520a0
zhangcui <zhangcui11@huawei.com>
2023-01-31 07:39:16 +0000

    update
    Signed-off-by: zhangcui <zhangcui11@huawei.com>

--- device/plugins/ftrace_plugin/tools/refresh_device_kernel_code.py
+++ device/plugins/ftrace_plugin/tools/refresh_device_kernel_code.py
@@ -34,7 +34,7 @@ def main():
     version = args.version
 
     version_path = "device_kernel_version/{}".format(version)
-    events_file = version_path + "/events"
+    events_file = "{}/events".format(version_path)
     if not Path(events_file).is_dir():
         logger.error('device kernel events directory does not exist({})'.format(events_file))
         exit(1)
