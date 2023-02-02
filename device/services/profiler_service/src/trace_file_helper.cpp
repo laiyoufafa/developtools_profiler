75b1dd0f1b73d838ff60637e91326e820c0ce5d7
zhangjian22 <zhangjian22@huawei.com>
2023-02-02 21:47:23 +0800

    fixed e9039cf from https://gitee.com/jakemiao/developtools_profiler/pulls/735 profiler抓取的trace文件smartperf打开不 Signed-off-by: 张建 <zhangjian22@huawei.com> #I6CLFK

--- device/services/profiler_service/src/trace_file_helper.cpp
+++ device/services/profiler_service/src/trace_file_helper.cpp
@@ -16,20 +16,13 @@
 
 #include <climits>
 #include <cstring>
-#if defined(is_mingw) && !is_mingw
 #include <openssl/sha.h>
-#endif
 #include <securec.h>
 #include "logging.h"
 
-TraceFileHelper::TraceFileHelper()
-#if defined(is_mingw) && !is_mingw
-    : shaCtx_(std::make_shared<SHA256_CTX>())
-#endif
+TraceFileHelper::TraceFileHelper() : shaCtx_(std::make_shared<SHA256_CTX>())
 {
-#if defined(is_mingw) && !is_mingw
     SHA256_Init(shaCtx_.get());
-#endif
 }
 
 TraceFileHelper::~TraceFileHelper()
@@ -41,43 +34,34 @@ bool TraceFileHelper::AddSegment(const uint8_t data[], uint32_t size)
     if (size > std::numeric_limits<decltype(header_.data_.length)>::max() - header_.data_.length - sizeof(size)) {
         return false;
     }
-#if defined(is_mingw) && !is_mingw
     int retval = 0;
     header_.data_.segments += 1;
-
     header_.data_.length += size;
     retval = SHA256_Update(shaCtx_.get(), data, size);
     CHECK_TRUE(retval, false, "[%u] SHA256_Update FAILED, s:%u, d:%p!", header_.data_.segments, size, data);
-#endif
     return true;
 }
 
 bool TraceFileHelper::Finish()
 {
-#if defined(is_mingw) && !is_mingw
     int retval = 0;
     retval = SHA256_Final(header_.data_.sha256, shaCtx_.get());
     CHECK_TRUE(retval, false, "[%u] SHA256_Final FAILED!", header_.data_.segments);
-#endif
     return true;
 }
 
 bool TraceFileHelper::Update(TraceFileHeader& header)
 {
-#if defined(is_mingw) && !is_mingw
     CHECK_TRUE(Finish(), false, "Finish FAILED!");
     if (memcpy_s(&header, sizeof(header), &header_, sizeof(header)) != 0) {
         return false;
     }
-#endif
     return true;
 }
 
 bool TraceFileHelper::Validate(const TraceFileHeader& header)
 {
-#if defined(is_mingw) && !is_mingw
     CHECK_TRUE(Finish(), false, "Finish FAILED!");
     return memcmp(&header_, &header, sizeof(header_)) == 0;
-#endif
     return true;
 }
