8afb7c312549121842867d94ebf6583e5033aba7
Chenshi <chenshi51@huawei.com>
2023-02-15 20:45:22 +0800

    初始化buff数组
    Signed-off-by: Chenshi <chenshi51@huawei.com>

--- hiebpf/test/unittest/ringbuffer_test.cpp
+++ hiebpf/test/unittest/ringbuffer_test.cpp
@@ -399,7 +399,7 @@ HWTEST_F(RingbufferTest, Peek, TestSize.Level1)
     len = ringBuffer->Put("3");
     EXPECT_EQ(len, 1);
     const int size = RingBuffer::BufferSize::DEFAULT_SIZE - 1;
-    char buff[size + 1];
+    char buff[size + 1] = {0};
     (void)memset_s(buff, size + 1, 0, size + 1);
     ret = ringBuffer->Get(buff, size);
     EXPECT_STRNE(buff, "");
