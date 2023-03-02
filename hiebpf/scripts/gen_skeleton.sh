b16790d21911041476c0953f3c48a31a13d79eee
zhangcui <zhangcui11@huawei.com>
2023-03-02 08:02:04 +0000

    清理告警
    Signed-off-by: zhangcui <zhangcui11@huawei.com>

--- hiebpf/scripts/gen_skeleton.sh
+++ hiebpf/scripts/gen_skeleton.sh
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License
 
+set -e
+
 BPFTOOL_DIR=${1}
 BASE_DIR=${2}
 DEST_DIR=${3}
@@ -33,7 +35,7 @@ then
     exit -1
 fi
 
-BPF_OBJ_SETS=`ls ${BASE_DIR} | grep '\.bpf'`
+BPF_OBJ_SETS=$(ls ${BASE_DIR} | grep '\.bpf')
 echo "bpf object directories: "${BPF_OBJ_SETS}
 
 for obj_set in ${BPF_OBJ_SETS}
@@ -41,7 +43,7 @@ do
     echo "current bpf object directory name: "${obj_set}
     BPF_OBJ_DIR=${BASE_DIR}"/"${obj_set}
     echo "current bpf object directory path: "${BPF_OBJ_DIR}
-    BPF_OBJECTS=`ls ${BPF_OBJ_DIR} | grep '\.bpf\.o'`
+    BPF_OBJECTS=$(ls ${BPF_OBJ_DIR} | grep '\.bpf\.o')
     echo "bpf object file names: "${BPF_OBJECTS}
     for obj in ${BPF_OBJECTS}
     do
