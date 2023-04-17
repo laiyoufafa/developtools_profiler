b0cf20dd454515dff9a53ad26d411834358ab17c
wenlong_12 <wenlong12@huawei.com>
2023-04-17 06:07:39 +0000

    告警清理
    Signed-off-by: wenlong_12 <wenlong12@huawei.com>

--- device/plugins/native_daemon/src/hook_standalone.cpp
+++ device/plugins/native_daemon/src/hook_standalone.cpp
@@ -151,7 +151,6 @@ void ReadShareMemory(uint64_t duration, const std::string& performance_filename)
             bool ret = g_runtimeInstance->UnwindStack(u64regs, rawData->stackData, rawData->stackSize,
                 rawData->stackConext->pid, rawData->stackConext->tid, callFrames,
                 (g_maxStackDepth > 0) ? g_maxStackDepth + FILTER_STACK_DEPTH : MAX_CALL_FRAME_UNWIND_SIZE);
-
             if (!ret) {
                 HILOG_ERROR(LOG_CORE, "unwind fatal error");
                 return false;
