830c597c9e094f7cc5c381efb2b2206d8be045ed
lwx1063336 <lixin416@huawei.com>
2023-05-24 09:36:03 +0800

    sp logo adapter & socket test
    Signed-off-by: lwx1063336 <lixin416@huawei.com>

--- host/smartperf/client/client_command/include/sp_thread_socket.h
+++ host/smartperf/client/client_command/include/sp_thread_socket.h
@@ -14,9 +14,11 @@
  */
 #ifndef SP_THREAD_SOCKET_H
 #define SP_THREAD_SOCKET_H
+#include <functional>
 #include "sp_profiler_factory.h"
 #include "sp_server_socket.h"
 #include "sp_utils.h"
+#include "sp_task.h"
 namespace OHOS {
 namespace SmartPerf {
 class SpThreadSocket {
@@ -36,32 +38,75 @@ public:
         }
         return result;
     }
-    std::string ResPkgOrPid(const SpServerSocket &spSocket) const
+    std::string SplitMsg(const std::string recvBuf) const
     {
         std::vector<std::string> sps;
-        SPUtils::StrSplit(spSocket.RecvBuf(), "::", sps);
+        SPUtils::StrSplit(recvBuf, "::", sps);
         return sps[1];
     }
 
-    void Process() const
+    void Process(ProtoType type) const
     {
+        std::cout << "Socket Process called!" << std::endl;
         SpServerSocket spSocket;
-        spSocket.Init();
-        while (1) {
-            spSocket.Recvfrom();
-            HandleMsg(spSocket);
+        spSocket.Init(type);
+        if (type == ProtoType::TCP) {
+            std::cout << "Socket TCP Init called!" << std::endl;
+            while (1) {
+                int procFd = spSocket.Accept();
+                std::cout << "Socket TCP procFd: " << procFd << std::endl;
+                while (procFd > 0) {
+                    spSocket.Recv();
+                    std::string recvStr = spSocket.RecvBuf();
+                    std::cout << "Socket TCP Recv: " << recvStr << std::endl;
+                    // 解析消息 分发处理
+                    DealMsg(recvStr, spSocket);
+                }
+            }
+        }
+        if (type == ProtoType::UDP) {
+            while (1) {
+                spSocket.Recvfrom();
+                HandleMsg(spSocket);
+            }
         }
         std::cout << "Socket Process finished!" << std::endl;
         spSocket.Close();
     }
+    // TCP
+    void DealMsg(std::string recvStr, SpServerSocket &spSocket) const
+    {
+        // init::-SessionId 12345678 -Interval 1000 -PKG ohos.samples.ecg -c -g -t -p -f -r
+        if (recvStr.find("init::") != std::string::npos) {
+            ErrCode code = SPTask::GetInstance().InitTask(SplitMsg(recvStr));
+            if (code == ErrCode::OK) {
+                spSocket.Send("init::True");
+            } else if (code == ErrCode::FAILED) {
+                spSocket.Send("init::False");
+            }
+        } else if (recvStr.find("start::") != std::string::npos) {
+            auto lambdaTask = [&spSocket](std::string data) { spSocket.Send(data); };
+            ErrCode code = SPTask::GetInstance().StartTask(lambdaTask);
+            if (code == ErrCode::OK) {
+                spSocket.Send("start::True");
+            } else if (code == ErrCode::FAILED) {
+                spSocket.Send("start::False");
+            }
+        } else if (recvStr.find("stop::") != std::string::npos) {
+            SPTask::GetInstance().StopTask();
+            spSocket.Send("stop::True");
+        }
+    }
+    // UDP
     void HandleMsg(SpServerSocket &spSocket) const
     {
         auto iterator = messageMap.begin();
         while (iterator != messageMap.end()) {
-            if (SPUtils::IsSubString(spSocket.RecvBuf(), iterator->second)) {
+            std::string recvBuf = spSocket.RecvBuf();
+            if (SPUtils::IsSubString(recvBuf, iterator->second)) {
                 SpProfiler *profiler = SpProfilerFactory::GetProfilerItem(iterator->first);
                 if (profiler == nullptr && (iterator->first == MessageType::SET_PKG_NAME)) {
-                    std::string curPkgName = ResPkgOrPid(spSocket);
+                    std::string curPkgName = SplitMsg(recvBuf);
                     SpProfilerFactory::SetProfilerPkg(curPkgName);
                     std::string pidCmd = "pidof " + curPkgName;
                     std::string pidResult;
@@ -70,7 +115,7 @@ public:
                     }
                     spSocket.Sendto(curPkgName);
                 } else if (profiler == nullptr && (iterator->first == MessageType::SET_PROCESS_ID)) {
-                    SpProfilerFactory::SetProfilerPid(ResPkgOrPid(spSocket));
+                    SpProfilerFactory::SetProfilerPid(SplitMsg(recvBuf));
                 } else if (profiler == nullptr) {
                     std::string returnStr = iterator->second;
                     spSocket.Sendto(returnStr);
